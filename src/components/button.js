d3f139d384339896c329cee736256315f799a47a
aalchen <43652006+aalchen@users.noreply.github.com>
2021-01-16 02:53:17 -0800

    landing page

---
+++ src/components/button.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+
+class button extends Component {
+  // state = {  }
+  render() {
+    return (
+      <div>
+        <Button variant="dark">Dark</Button>
+      </div>
+    );
+  }
+}
+
+export default button;
