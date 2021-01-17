52278bd7793b1d4dbdb6228d27ea66da66656120
Jerome <jerometing1@gmail.com>
2021-01-17 00:44:12 -0800

    what is life

--- src/components/client.js
+++ src/components/client.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import DisplayTable from './DisplayTable';
+import DisplayTable2 from './DisplayTable2'
+import PDFUpload from './PDFUpload';
 
 const client = () => {
     return (
         <div>
             <h1>This is the client page</h1>
+
+            <PDFUpload/>
         </div>
     )
 }
