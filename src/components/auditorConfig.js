cd445739dfb702148b193b68d9ec44a97d62a50e
Jerome <jerometing1@gmail.com>
2021-01-16 16:42:54 -0800

    Added the upload with the link to a local server

--- src/components/auditorConfig.js
+++ src/components/auditorConfig.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
+import FileReader from './FileReader';
+//import fileuploader1 from './fileuploader1';
 
 const AuditorConfig = () => {
     return (
         <div>
             <Navbar/>
             <h1>This is Auditor Configuration Page</h1>
+            <FileReader/>
         </div>
     )
 }
